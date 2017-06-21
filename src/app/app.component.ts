7accb346d3d3c984ce7bfa18e72bb381a3346250
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2017-06-21 11:53:32 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <user></user>
+   `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+ }
